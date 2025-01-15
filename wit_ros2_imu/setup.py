bcda290783d7140f6c75d8a359bb40a4a1bde8ee
hsyoon <hsyoon94@snu.ac.kr>
2025-01-15 20:28:31 +0900

    imu added but 10hz

--- wit_ros2_imu/setup.py
+++ wit_ros2_imu/setup.py
@@ -1,28 +1,28 @@
-from setuptools import find_packages, setup
+from setuptools import setup
+import os, glob
 
-package_name = 'data_transmitter'
+package_name = 'wit_ros2_imu'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' +package_name, ['launch/rviz_and_imu.launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='viver1',
-    maintainer_email='viver1@todo.todo',
+    maintainer='pi',
+    maintainer_email='pi@todo.todo',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'data_sender = data_transmitter.imu_processor:main',
-            'stereo_image_processor = data_transmitter.stereo_image_processor:main',
-            'image_sender = data_transmitter.image_sender:main',
+        'wit_ros2_imu = wit_ros2_imu.wit_ros2_imu:main'
         ],
     },
 )
